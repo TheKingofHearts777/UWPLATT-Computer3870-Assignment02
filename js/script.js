d5b835caea3f56f606d35f329f8c71d5cf338e03
TheKingofHearts777 <dbrodie678@gmail.com>
2025-10-21 19:20:12 +0000

    fetch request beginnings

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const emojiJSON = fetch("https://thekingofhearts777.github.io/UWPLATT-Computer3870-Assignment02-json-data/emoji.json")
+.then()
+.then()
+.catch();
+
+
+function display(data) {
+    let emojiJSONdiv = document.createElement("div");
+
+    emojiJSONdiv.innerHTML = `
+        ${emojiJSON}
+    `
+}
